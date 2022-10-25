116a4148273cb632a1d0a2d9b95e2ffccb55a734
Max Clarke <maximilian-jc@live.com>
2022-10-25 21:18:15 +0100

    added the code that needs to be in backend into service for now not working;

--- my-app/src/services/ItemsService.js
+++ my-app/src/services/ItemsService.js
@@ -1,2 +1,14 @@
 // Make request to api backend to the below url
 // API: http://cdn.merakianalytics.com/riot/lol/resources/latest/en-US/items/
+
+const { default: axios } = require('axios');
+
+axios.get('http://cdn.merakianalytics.com/riot/lol/resources/latest/en-US/items/')
+    .then(response => {
+        return response.data;
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
+    //above request needs to be in backend and this service make a call to backend to get the data
